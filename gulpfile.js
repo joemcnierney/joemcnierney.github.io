cc4e7d291253c51a2bc5c0ee09ef0bfd6e371f4c
joemcnierney <joemcnierney@gmail.com>
2020-01-31 06:53:10 -0700

    Asset Upload

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2511
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red162\green127\blue173;\red185\green188\blue186;\red111\green144\blue176;
+\red212\green128\blue77;\red22\green23\blue25;\red132\green134\blue132;}
+{\*\expandedcolortbl;;\cssrgb\c69804\c58039\c73333;\cssrgb\c77255\c78431\c77647;\cssrgb\c50588\c63529\c74510;
+\cssrgb\c87059\c57647\c37255;\cssrgb\c11373\c12157\c12941;\cssrgb\c58824\c59608\c58824;}
+\margl1440\margr1440\vieww10800\viewh10200\viewkind0
+\deftab720
+\pard\pardeftab720\sl320\partightenfactor0
+
+\f0\fs27\fsmilli13600 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 function\cf3 \strokec3  \cf4 \strokec4 defaultTask\cf3 \strokec3 (\cf5 \strokec5 cb\cf3 \strokec3 ) \cb6 \{\
+  \cf7 \cb1 \strokec7 // place code for your default task here\cf3 \cb6 \strokec3 \
+  cb();\
+\}\
+\
+exports.default = defaultTask}
